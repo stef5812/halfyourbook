718e0f7dff409ebd295dad0d7ead75a1306121dd
Stef5812 <stefandodds@hotmail.com>
2026-02-05 10:46:57 +0000

    Describe your changes clearly

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/halfyourbook/",
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:3004",
-        changeOrigin: true,
-      },
-      "/uploads": {
-        target: "http://127.0.0.1:3004",
-        changeOrigin: true,
+export default defineConfig(({ command }) => {
+  const isBuild = command === "build";
+
+  return {
+    plugins: [react()],
+    base: "/halfyourbook/",
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://127.0.0.1:3004",
+          changeOrigin: true,
+        },
+        "/uploads": {
+          target: "http://127.0.0.1:3004",
+          changeOrigin: true,
+        },
       },
     },
-  },
+    build: isBuild
+      ? {
+          outDir: "/var/www/stefandodds.ie/halfyourbook",
+          emptyOutDir: true,
+        }
+      : undefined,
+  };
 });
